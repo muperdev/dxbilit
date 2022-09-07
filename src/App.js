25a19d056e5e2667554c925c97153421b848bc0c
mupermusic <behrooz.records@gmail.com>
2022-09-07 17:36:29 +0430

    fix: route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import Blogs from './pages/blogs';
 import Menu from './components/menu';
 import { BlogPage } from './pages/blog';
+import HomePage from './pages/homepage';
+import Product from './pages/product';
 
 function App() {
   return (
@@ -10,9 +12,10 @@ function App() {
       <Menu />
       <main>
         <Routes>
-          <Route exact path="/" element={<Blogs />} />
+          <Route exact path="/" element={<HomePage />} />
           <Route exact path="/blog" element={<Blogs />} />
           <Route exact path="/blog/id" element={<BlogPage />} />
+          <Route exact path="/product" element={<Product />} />
         </Routes>
       </main>
     </div>
